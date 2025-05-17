d578c6ab9638132214ef5e4f5ad4e26908ef710f
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2025-05-17 15:20:34 -0300

    fix: remove loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <AuthProvider>
           <CartContextProvider>
             <Toaster />
-            <Suspense fallback={<Loading />}>
+            <Suspense>
               <Header />
             </Suspense>
             {children}
